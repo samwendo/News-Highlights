296d5de01a6b6f922b61e91101e2fc25e0b384c7
samwendo <wendosam21@gmail.com>
2019-10-14 15:37:15 +0300

    first

---
+++ manage.py
@@ -0,0 +1,11 @@
+from app import create_app
+from flask_script import Manager, Server
+
+app = create_app("development")
+
+manage = Manager(app)
+
+manage.add_command("server", Server)
+
+if __name__ == "__main__":
+    manage.run()
