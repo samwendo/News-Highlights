967f4dee9aa7972cf1d0b8c1dc76c3d557ad4552
samwendo <wendosam21@gmail.com>
2019-10-15 10:16:56 +0300

    form config

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,25 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-# from config import config_options
+from config import config_options
 
 bootstrap = Bootstrap()
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
-    #creating the app configurations
-    # app.config.from_object(config_options[config_name])
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-    #initializing flask extensions
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
-    #registering the Blueprint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    #setting config
+    # setting config
     from .request import configure_request
-    # configure_request(app)
-
+    configure_request(app)
 
     return app
